bfe094adb8dc9a8e6ceabd5fd06d8641dfe03bae
Jeff Daze <jeffrey.daze@gmail.com>
2024-11-11 12:06:59 -0600

    Changing default fonts to OpenDyslexicM Nerd Font

--- fonts.lua
+++ fonts.lua
@@ -5,16 +5,16 @@ local M = {}
 function M.setup(config)
 	config.font = wezterm.font_with_fallback({
 		{
-			family = "Hack Nerd Font",
+			family = "OpenDyslexicM Nerd Font",
 		},
 	})
-	config.font_size = 9
+	config.font_size = 10
 	-- config.underline_thickness = "200%"
 	-- config.underline_position = "-3pt"
 	-- config.adjust_window_size_when_changing_font_size = false
 	config.window_frame = {
-		font = wezterm.font({ family = "Hack Nerd Font", weight = "Regular" }),
-		font_size = 9,
+		font = wezterm.font({ family = "Hasklug Nerd Font", weight = "Regular" }),
+		font_size = 12,
 	}
 end
 
